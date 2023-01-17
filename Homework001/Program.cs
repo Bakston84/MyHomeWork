6562dfe53046e6e8af3d47b85bdb7a81e2ec0ed7
Bakston84 <Bakston1@yandex.ru>
2023-01-17 12:39:30 +0500

    adding a solution to the problem

--- Homework001/Program.cs
+++ Homework001/Program.cs
@@ -1,4 +1,19 @@
 ﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//  Пример:
 //  a = 5; b = 7 -> max = 7
 //  a = 2 b = 10 -> max = 10
 //  a = -9 b = -3 -> max = -3
+
+System.Console.Write("Введите первое число > ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите первое число > ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    System.Console.WriteLine($"Первое число ({firstNumber}), больше второго числа ({secondNumber})");
+}
+else
+{
+    System.Console.WriteLine($"Второе число ({secondNumber}) больше первого числа ({firstNumber})");
+}

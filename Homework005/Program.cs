fd9b21dc97c3c8ad4407230fe910ade3fddcb5e7
Bakston84 <Bakston1@yandex.ru>
2023-01-18 12:19:47 +0500

    adding a solution to the problem homework005

---
+++ Homework005/Program.cs
@@ -0,0 +1,30 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//  Пример:
+//  456 -> 5
+//  782 -> 8
+//  918 -> 1
+
+int InputInt(string message)
+{
+    System.Console.Write(message + " > ");
+    string inputValue = System.Console.ReadLine();
+    int result = Convert.ToInt32(inputValue);
+    return result;
+}
+
+bool ValidateNumberRank2(int number)
+{
+    if (number > 99 && number < 10000)
+    {
+        return true;
+    }
+    System.Console.WriteLine($"Число {number} не является 3-хзначным числом");
+    return false;
+}
+
+int number = InputInt("Введите 3-хзначное число");
+if (ValidateNumberRank2(number))
+{
+    int result = number / 10 % 10;
+    System.Console.WriteLine($"Вторая цифра числа {number} равна ({result})");
+}

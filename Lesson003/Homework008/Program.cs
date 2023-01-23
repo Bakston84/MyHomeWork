777ee3e33736076fe1df77300791e1a49001c5be
Bakston84 <Bakston1@yandex.ru>
2023-01-23 10:51:56 +0500

    adding a solution to the problem homework008

--- Lesson003/Homework008/Program.cs
+++ Lesson003/Homework008/Program.cs
@@ -3,3 +3,37 @@
 //  14212 -> нет
 //  12821 -> да
 //  23432 -> да
+
+string InputString(string message)
+{
+    System.Console.Write(message + " > ");
+    string? inputValue = System.Console.ReadLine();
+    return inputValue;
+}
+
+bool ValidateNumber(string number)
+{
+    int rank = number.Length;
+    if (rank == 5)
+    {
+        return true;
+    }
+    Console.WriteLine($"Число {number} не является пятизначным");
+    return false;
+}
+
+string number = InputString("Введите пятизначное число");
+if (ValidateNumber(number))
+{
+    char[] revNumber = number.ToCharArray();
+    Array.Reverse(revNumber);
+    string finalNumber = new String(revNumber);
+    if (number == finalNumber)
+    {
+        System.Console.WriteLine($"Число {number} является палиндромом");
+    }
+    else
+    {
+        System.Console.WriteLine($"Число {number} не является палиндромом");
+    }
+}

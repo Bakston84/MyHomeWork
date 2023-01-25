ba130649ec29dda708a2f658781b702ab3a81dc2
Bakston84 <Bakston1@yandex.ru>
2023-01-25 16:39:39 +0500

    adding a solution to the problem homework011

---
+++ Lesson004/Homework012/Program.cs
@@ -0,0 +1,40 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//  Пример:
+//  452 -> 11
+//  82 -> 10
+//  9012 -> 12
+
+int InputInt(string message)
+{
+    System.Console.Write(message + " > ");
+    string? inputValue = System.Console.ReadLine();
+    int result = Convert.ToInt32(inputValue);
+    return result;
+}
+
+int SumDigitsNumber(int number)
+{
+    if (number <= 9)
+        return number;
+
+    int size = 0;
+    int index = number;
+    while (index > 0)
+    {
+        index = index / 10;
+        size++;
+    }
+
+    int result = 0;
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        result = number % 10;
+        sum = sum + result;
+        number /= 10;
+    }
+    return sum;
+}
+
+int number = InputInt("Введите число");
+System.Console.WriteLine($"Сумма цифер числа {number} = {SumDigitsNumber(number)}");

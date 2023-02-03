38d2e25f2e526f948d489d825deed07342da7ed1
Bakston84 <Bakston1@yandex.ru>
2023-02-03 10:46:04 +0500

    03022023_1046

---
+++ Lesson006/Homework018/Program.cs
@@ -0,0 +1,24 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Пример:
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int InputInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int CheckPositivity(int result)
+{
+    for (int i = 1; i < 6; i++)
+    {
+        int number = InputInt($"Введите {i} число");
+        if (number > 0)
+        result++;
+    }
+    return result;
+}
+
+int result = 0;
+System.Console.WriteLine($"Чисел больше 0: {CheckPositivity(result)}");
---
+++ Lesson006/Homework019/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// Пример:
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int InputInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void InputPoint(double[,] inputPoints)
+{
+    for (int i = 0; i < inputPoints.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputPoints.GetLength(1); j++)
+        {
+            if (i == 0) System.Console.Write($"Введите точку k.");
+            else System.Console.Write($"Введите точку b.");
+            inputPoints[i, j] = InputInt($"{j + 1}");
+        }
+    }
+}
+
+double[,] inputPoints = new double[2, 2];
+InputPoint(inputPoints);
+double x = (inputPoints[1, 1] - inputPoints[1, 0]) / (inputPoints[0, 0] - inputPoints[0, 1]);
+double y = inputPoints[0, 0] * x + inputPoints[1, 0];
+System.Console.WriteLine($"Точкой пересечения двух прямых является: {x}; {y}");

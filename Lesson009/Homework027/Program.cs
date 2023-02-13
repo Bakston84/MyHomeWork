2fb2befc3b8331d7a49b670ab0b592e9a381f663
bakston84 <bakson1@yandex.ru>
2023-02-13 11:56:34 +0500

    13022023_1156

--- Lesson009/Homework027/Program.cs
+++ Lesson009/Homework027/Program.cs
@@ -4,3 +4,26 @@
 // M = 1; N = 5 -> "2, 4"
 // M = 4; N = 8 -> "4, 6, 8"
 
+int InputInt(string number)
+{
+    System.Console.Write($"{number} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void SearchPrivateNumbers(int m, int n)
+{
+    if (m > n) return;
+    if (m % 2 == 0) PrintResult(m);
+    m++;
+    SearchPrivateNumbers(m, n);
+}
+
+void PrintResult (int m)
+{
+    System.Console.Write($"({m}) ");
+}
+
+int numberM = InputInt("Введите первое число");
+int numberN = InputInt("Введите второе число");
+System.Console.Write($"Чётные натуральные числа: ");
+SearchPrivateNumbers(numberM, numberN);
--- Lesson009/Homework028/Program.cs
+++ Lesson009/Homework028/Program.cs
@@ -4,3 +4,31 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+int InputInt(string number)
+{
+    System.Console.Write($"{number} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void SumNumbers(int m, int n, int result)
+{
+    if (m == n)
+    {
+        PrintResult(result);
+        return;
+    }
+    m++;
+    result = result + m;
+    SumNumbers(m, n, result);
+}
+
+void PrintResult(int m)
+{
+    System.Console.Write($"({m}) ");
+}
+
+int numberM = InputInt("Введите первое число");
+int numberN = InputInt("Введите второе число");
+int result = numberM;
+System.Console.Write($"Сумма натуральных чисел: ");
+SumNumbers(numberM, numberN, result);
